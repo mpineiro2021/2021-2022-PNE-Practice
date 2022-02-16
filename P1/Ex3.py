4844f8412a99ca1fe3ac62545383a08bf26ed708
marta <m.pineiro.2021@alumnos.urjc.es>
2022-02-16 18:33:03 +0100

    other change

--- P1/Ex3.py
+++ P1/Ex3.py
@@ -7,3 +7,8 @@ s2 = Seq("ACTGA")
 
 # -- Create an invalid sequence
 s3 = Seq("Invalid sequence")
+
+print("----| Exercise 3 |------")
+print("Sequence 1:",s1)
+print("Sequence 2:", s2)
+print("Sequence 2:", s3)
