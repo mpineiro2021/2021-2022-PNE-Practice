2cc4f0007f00491ae8f6c5d07ec6f759f64057ac
marta <m.pineiro.2021@alumnos.urjc.es>
2022-03-21 11:48:12 +0100

    other change

--- P3/server.py
+++ P3/server.py
@@ -10,6 +10,10 @@ ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 PORT = 8080
 IP = "127.0.0.1"
 
+def get_command(gene_number):
+    #copiar el elif
+return response
+
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -38,26 +42,28 @@ while True:
     # -- We decode it for converting it
     # -- into a human-redeable string
     msg = msg_raw.decode().replace("\n","").strip()
-    splitted_command = msg.split(" ")
-    cmd = splitted_command[0]
-    if cmd != "PING":
-        arg = splitted_command[1]
-        print(arg)
+    try:
+        splitted_command = msg.split(" ")
+        cmd = splitted_command[0]
+        if cmd != "PING":
+            arg = splitted_command[1]
+            print(arg)
 
-    # -- Print the received message
-    print(f" {msg} command!!")
-    if cmd == "PING":
-        response = "Ok!!!\n"
-        print(response)
-    elif cmd == "GET":
-        seq_list = ["AACTG","AACCCCTTTGGG","CCCTG","CCCTGA","AAAAA"]
-        response= seq_list[int(arg)]
-    elif cmd == "INFO":
-        response
-    else:
-    # -- Send a response message to the client
-        response = "This command is not available in the server\n"
-
+        # -- Print the received message
+        print(f" {msg} command!!")
+        if cmd == "PING":
+            response = "Ok!!!\n"
+            print(response)
+        elif cmd == "GET":
+            seq_list = ["AACTG","AACCCCTTTGGG","CCCTG","CCCTGA","AAAAA"]
+            response= seq_list[int(arg)]
+        elif cmd == "INFO":
+            response
+        else:
+        # -- Send a response message to the client
+            response = "This command is not available in the server\n"
+    except Exception:
+        response = f"ERROR : {command}\n"
     # -- The message has to be encoded into bytes
     cs.send(response.encode())
 
