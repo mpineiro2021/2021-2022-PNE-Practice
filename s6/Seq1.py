059a1ff5730b374f2a7f6b2f5c9501b980c7ce84
marta <m.pineiro.2021@alumnos.urjc.es>
2022-03-22 17:00:24 +0100

    other change

--- s6/Seq1.py
+++ s6/Seq1.py
@@ -1,45 +1,55 @@
 class Seq:
-    """A class for representing sequences"""
+    BASES_ALLOWED= ["A","C","G","T"]
 
-    def __init__(self, strbases):#no se retorna nada en la init function
-
-        # Initialize the sequence with the value
-        # passed as argument when creating the object
-        self.strbases = strbases
-        if not self.validate_sequence():
-            self.strbases = "Error"
-            print("ERROR!!")
-        else:
+    def __init__(self,bases):#no se retorna nada en la init function
+        if Seq.validate_sequence(bases):
+            self.bases = bases
             print("New sequence created!")
+        else:
+            self.bases = "ERROR"
+            print("Incorrect sequence detected!")
 
     @staticmethod
-    def validate_sequence2(sequence):
-        valid = True
+    def validate_sequence(bases):
+        valid = len(bases) != 0
         i = 0
-        while i < len(sequence) and valid:
-            c = sequence[i]
-            if c != "A" and c != "C" and c != "G" and c != "T":
+        while i < len(bases) and valid:
+            if bases[i] in Seq.BASES_ALLOWED:
+                i += 1
+            else:
                 valid = False
-            i += 1
         return valid
 
 
-    def validate_sequence(self):
+    '''def validate_sequence(self):
         valid = True
         i = 0
-        while i < len(self.strbases) and valid:
-            c = self.strbases[i]
+        while i < len(self.bases) and valid:
+            c = self.bases[i]
             if c != "A" and c != "C" and c != "G" and c != "T":
                 valid = False
             i += 1
-        return valid
+        return valid'''
 
     def __str__(self):
-        """Method called when the object is being printed"""
-
         # -- We just return the string with the sequence
-        return self.strbases
+        return self.bases
 
     def len(self):
         """Calculate the length of the sequence"""
-        return len(self.strbases)
+        return len(self.bases)
+
+'''class Gene(Seq):
+    def __init__(self, bases, name=""):
+        super().__init__(bases)
+        self.name = name
+        print("New gene created!!!")
+    def __str__(self):
+        return self.name +"-"+self.bases
+
+s1= Seq("AGTACACTGGT")
+g = Gene("CGTAAC","FRAT1")
+print(f"Sequence 1: {s1}")
+print(f"  Length: {s1.len()}")
+print(f"Gene: {g}")
+print(f"  Length: {g.len()}")'''
--- s6/ex1.py
+++ s6/ex1.py
@@ -1,22 +1,5 @@
-class Seq:
-    """A class for representing sequences"""
-    def __init__(self, strbases):
-        self.strbases = strbases #attribute called strbases
-        print("New sequence created!")
-    def __str__(self):
-        """Method called when the object is being printed"""
-
-        # -- We just return the string with the sequence
-        return self.strbases
-
-
-#Main program
-#Create an object of the class Seq
-s1 = Seq("AGTACACTGGT")
-# Create another object of the Class Seq
-s2 = Seq("CGTAAC")
-
-# -- Printing the objects
+from Seq1 import *
+s1= Seq("AGTACACTGGT")
+s2 = Seq("Am I a valid sequence?")
 print(f"Sequence 1: {s1}")
 print(f"Sequence 2: {s2}")
-print("Testing....")
